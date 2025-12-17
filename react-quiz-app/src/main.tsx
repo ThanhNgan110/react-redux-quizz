b353c1ea12ec224a83f62d5158a6bad0dbc839eb
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2025-12-17 22:02:57 +0700

    add

--- react-quiz-app/src/main.tsx
+++ react-quiz-app/src/main.tsx
@@ -5,11 +5,10 @@ import { store, persistor } from './store.ts'
 import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')!).render(
-	// <StrictMode>
+
 	<Provider store={store}>
 		<PersistGate loading={null} persistor={persistor}>
 			<App />
 		</PersistGate>
-	</Provider>,
-	//  </StrictMode>
+	</Provider>
 )

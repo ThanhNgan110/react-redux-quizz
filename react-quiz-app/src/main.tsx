cbe204150e7c74c2d449de722ad5e8a987087cf5
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2025-12-17 22:05:34 +0700

    add strictmode

--- react-quiz-app/src/main.tsx
+++ react-quiz-app/src/main.tsx
@@ -1,3 +1,4 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
@@ -5,10 +6,12 @@ import { store, persistor } from './store.ts'
 import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')!).render(
-
-	<Provider store={store}>
-		<PersistGate loading={null} persistor={persistor}>
-			<App />
-		</PersistGate>
-	</Provider>
+	<StrictMode>
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				<App />
+			</PersistGate>
+		</Provider>
+		,
+	</StrictMode>,
 )
